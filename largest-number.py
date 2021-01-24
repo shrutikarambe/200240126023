de0c912598e7e1271601b34cb753745aafd0d12f
shruti karambe <rekha.r.more@gmail.com>
2021-01-24 10:52:04 +0530

    Largest number

---
+++ largest-number.py
@@ -0,0 +1,6 @@
+print("Enter 3 number to find largest among them")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+print(f'The largest number is: {max(num1, num2, num3)}')
